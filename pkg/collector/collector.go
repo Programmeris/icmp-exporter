18997f33a771bc6049396568476530cb42742632
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2023-12-03 20:43:44 +0000

    implement collector interface

--- pkg/collector/collector.go
+++ pkg/collector/collector.go
@@ -8,3 +8,22 @@ import (
 type IcmpCollector struct {
 	addressAvailableByIcmp *prometheus.Desc
 }
+
+func NewIcmpCollector() *IcmpCollector {
+	return &IcmpCollector{
+		addressAvailableByIcmp: prometheus.NewDesc("address_available_by_icmp",
+			"address availability by icmp",
+			nil, nil,
+		),
+	}
+}
+
+func (collector *IcmpCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- collector.addressAvailableByIcmp
+}
+
+//Collect implements required collect function for all promehteus collectors
+func (collector *IcmpCollector) Collect(ch chan<- prometheus.Metric) {
+	m1 := prometheus.MustNewConstMetric(collector.addressAvailableByIcmp, prometheus.GaugeValue, 1)
+	ch <- m1
+}
