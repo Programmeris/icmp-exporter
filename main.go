cc077775b988107ae11176ee1954a2a46dd982e5
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2023-12-03 20:53:20 +0000

    init icmp collector in main package

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-    "net/http"
-    "github.com/prometheus/client_golang/prometheus/promhttp"
+	. "icmp-exporter/pkg/collector"
+	"net/http"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+    icmpCollector := NewIcmpCollector()
+    prometheus.MustRegister(icmpCollector)
+
     http.Handle("/metrics", promhttp.Handler())
     http.ListenAndServe(":2112", nil)
 }
